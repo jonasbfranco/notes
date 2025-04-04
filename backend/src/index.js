dd0bfcd98ef31e8e9e2ff5ac990b2c85d0fd1ca3
Jonas Baptista Franco <jonas@MacBook-Air-de-Jonas.local>
2025-04-04 20:18:01 -0300

    Aula - 3 - Frontend - 04/04/2025

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const prisma = require("./config/prisma");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,19 +1,67 @@
-import Notes from "./components/notes/notes";
+"use client";
+
+import { useState } from "react";
+import Notes from "@/app/components/notes/notes";
+import api from "@/services/api";
+
+type Note = {
+  id: string;
+  title: string;
+  notes: string;
+  priority: boolean;
+}
 
 export default function Home() {
+  
+  const [selectedValue, setSelectedValue] = useState("all");
+  const [title, setTitles] = useState("");
+  const [notes, setNotes] = useState("");
+  const [allNotes, setAllNotes] = useState<Note[]>([]);
+
+
+  async function handleSubmit(e: any) {
+    e.preventDefault();
+    // return console.log(title, notes);
+
+    const response = await api.post("/annotations", {
+      title,
+      notes,
+      priority: false,
+    });
+
+    setTitles("");
+    setNotes("");
+
+
+  };
+
+
+
   return (
     <>
       <div id="app">
         <aside>
           <strong>Caderno de Notas</strong>
-          <form action="">
+          <form onSubmit={handleSubmit}>
             <div className="input-block">
               <label htmlFor="title">Título da Anotação</label>
-              <input type="text" id="title" name="title" required maxLength={30}/>
+              <input 
+                type="text" 
+                id="title" 
+                name="title" 
+                required 
+                maxLength={30}
+                value={title}
+                onChange={(e) => setTitles(e.target.value)}
+              />
             </div>
             <div className="input-block">
               <label htmlFor="nota">Anotação</label>
-              <textarea required />
+              <textarea 
+                required
+                value={notes}
+                onChange={(e) => setNotes(e.target.value)} 
+              />
             </div>
             <button id="btn_submit" type="submit">Salvar</button>
           </form>
