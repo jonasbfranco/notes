21bd0ff0328c060336866ad154ba749d26369f1b
Jonas Baptista Franco <jonas@MacBook-Air-de-Jonas.local>
2025-04-21 16:56:09 -0300

    21/04/2025 - Frontend - modificar nota no card :sparkles:

--- frontend/src/app/components/notes/notes.tsx
+++ frontend/src/app/components/notes/notes.tsx
@@ -2,6 +2,8 @@ import { AiFillDelete, AiOutlineExclamationCircle } from "react-icons/ai";
 
 import "./styles.css";
 import "./styles-priority.css";
+import { useState } from "react";
+import api from "@/services/api";
 
 type NoteProps = {
     note: {
@@ -18,6 +20,34 @@ export default function Notes(props: NoteProps) {
 
     const { note, handleDelete, handleChangePriority} = props;
 
+    const [changeNote, setChangeNote] = useState("");
+
+
+    function handleEdit(e: HTMLTextAreaElement, priority: boolean) {
+        e.style.cursor = "text";
+        e.style.borderRadius = "5px";
+
+        if (priority) {
+            e.style.boxShadow = "0 0 5px #8249d2"
+        } else {
+            e.style.boxShadow = "0 0 5px #dbdbdb"
+        }
+    };
+
+    
+    async function handleSave(e: HTMLTextAreaElement, notes: string) {
+        e.style.cursor = "default";
+        e.style.boxShadow = "none";
+
+        if (changeNote && changeNote !== notes) {
+            await api.put(`/contents/${props.note.id}`, {
+                notes: changeNote,
+            })
+        }
+        setChangeNote("");
+    };
+
+
     return (
         <>
             <li className={props.note.priority ? "notepad-infos-priority" : "notepad-infos"}>
@@ -32,6 +62,10 @@ export default function Notes(props: NoteProps) {
                 </div>
                 <textarea
                     defaultValue={props.note.notes}
+                    onClick={ (e) => handleEdit(e.target, props.note.priority) }
+                    onChange={ (e) => setChangeNote(e.target.value) }
+                    onBlur={ (e) => handleSave(e.target, props.note.notes) }
+
                 />
                 <span>
                     <AiOutlineExclamationCircle 
